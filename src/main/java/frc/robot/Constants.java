e9f3aa7d0dcf0c371a13589eb2ded1b7258cf632
joey-frc5675 <>
2026-02-19 19:30:31 -0500

    Add preliminary pose data

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,7 @@ import com.pathplanner.lib.path.PathConstraints;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 
 public class Constants {
 
@@ -25,13 +26,39 @@ public class Constants {
             Units.degreesToRadians(360), Units.degreesToRadians(540));
     }
     public class PoseConstants {
+        // Values are in Meters
+        public static final double BLUE_TOWER_X = 1.065;
+        public static final double RED_TOWER_X = 15.48;
 
-        public static final Pose2d BLUE_TOWER_OUTPOST = new Pose2d(1.247,5.430,new Rotation2d(90));
-        public static final Pose2d BLUE_TOWER_NOT_OUTPOST = new Pose2d(1.247, 1.744, new Rotation2d(270));
+        public static final double BLUE_TOWER_OUTPOST_Y = 4.700;
+        public static final double RED_TOWER_OUTPOST_Y = 5.265;
 
+        public static final double BLUE_TOWER_DEPOT_Y = 4.700;
+        public static final double RED_TOWER_DEPOT_Y = 3.4; 
+
+        public static final double BLUE_STAGING_OFFSET = 0.5;
+        public static final double RED_STAGING_OFFSET = 0.5;
+
+        public static final double DEPO_DIFFERENCE = 0.1;
+        
+        public static final Pose2d BLUE_TOWER_OUTPOST = new Pose2d(BLUE_TOWER_X, BLUE_TOWER_OUTPOST_Y, Rotation2d.fromDegrees(90));
+        public static final Pose2d BLUE_TOWER_OUTPOST_STAGING = new Pose2d(BLUE_TOWER_X, BLUE_TOWER_OUTPOST_Y + BLUE_STAGING_OFFSET, Rotation2d.fromDegrees(90));
+
+        public static final Pose2d BLUE_TOWER_DEPOT = new Pose2d(BLUE_TOWER_X, BLUE_TOWER_DEPOT_Y, Rotation2d.fromDegrees(270));
+        public static final Pose2d BLUE_TOWER_DEPOT_STAGING = new Pose2d(BLUE_TOWER_X, BLUE_TOWER_OUTPOST_Y - (BLUE_STAGING_OFFSET + DEPO_DIFFERENCE), Rotation2d.fromDegrees(270));
+        
+        public static final Pose2d RED_TOWER_OUTPOST = new Pose2d(RED_TOWER_X, RED_TOWER_OUTPOST_Y, Rotation2d.fromDegrees(90));
+        public static final Pose2d RED_TOWER_OUTPOST_STAGING = new Pose2d(RED_TOWER_X, RED_TOWER_OUTPOST_Y - RED_STAGING_OFFSET, Rotation2d.fromDegrees(90));
+        
+        public static final Pose2d RED_TOWER_DEPOT = new Pose2d(RED_TOWER_X, RED_TOWER_DEPOT_Y, Rotation2d.fromDegrees(270));
+        public static final Pose2d RED_TOWER_DEPOT_STAGING = new Pose2d(RED_TOWER_X, RED_TOWER_DEPOT_Y + (RED_STAGING_OFFSET - DEPO_DIFFERENCE), Rotation2d.fromDegrees(270));
+
+        
+        public static final Translation2d BLUE_HUB_CENTER = new Translation2d(4.626, 4.035);
+        public static final Translation2d RED_HUB_CENTER = new Translation2d(11.915, 4.035);
     }
     public class IndexerConstants {
-        public static final int indexerMotorId = 32;
+        public static final int indexerMotorId = 0;
         
     }
     public class ClimberConstants {
@@ -39,13 +66,13 @@ public class Constants {
         
     }
     public class IntakeConstants {
-        public static final int intakeMotorId = 30;
-        public static final int extendMotorId = 31;
+        public static final int intakeMotorId = 0;
+        public static final int extendMotorId = 0;
     }
     public class ShooterConstants {
-        public static final int flyWheelMotorId = 15; //left
-        public static final int followerMotorId = 16;
-        public static final int providerMotorId = 17;
-        public static final int hoodMotorId = 18;
+        public static final int flyWheelMotorId = 0;
+        public static final int followerMotorId = 0;
+        public static final int providerMotorId = 0;
+        public static final int hoodMotorId = 0;
     }
 }
